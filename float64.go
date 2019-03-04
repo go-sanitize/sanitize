1926a17f4f9250e4f0a894a0ffe0f2901b91733c
hscasn <henrique.coelho@groupbyinc.com>
2019-03-04 14:11:45 -0500

    removed redundant code

--- float64.go
+++ float64.go
@@ -28,73 +28,73 @@ func sanitizeFloat64Field(s Sanitizer, structValue reflect.Value, idx int) error
 		}
 	}
 
+	var err error
+
+	// Minimum value
+	_, hasMin := tags["min"]
+	min := float64(0)
+	if hasMin {
+		min, err = parseFloat64(tags["min"])
+		if err != nil {
+			return err
+		}
+	}
+
+	// Maximum value
+	_, hasMax := tags["max"]
+	max := float64(0)
+	if hasMax {
+		max, err = parseFloat64(tags["max"])
+		if err != nil {
+			return err
+		}
+	}
+
+	// Checking if minimum is not higher than maximum
+	if hasMax && hasMin && max < min {
+		return fmt.Errorf(
+			"max less than min on float64 field '%s' during struct sanitization",
+			fieldValue.Type().Name(),
+		)
+	}
+	// Checking if minimum and maximum are above 0
+	if (hasMin && min < 0) || (hasMax && max < 0) {
+		return fmt.Errorf(
+			"min and max on float64 field '%s' can not be below 0",
+			fieldValue.Type().Name(),
+		)
+	}
+
+	// Default value
+	_, hasDef := tags["def"]
+	def := float64(0)
+	if hasDef {
+		def, err = parseFloat64(tags["def"])
+		if err != nil {
+			return err
+		}
+
+		// Making sure default is not smaller than min or higher than max
+		if hasMax && def > max {
+			return fmt.Errorf(
+				"incompatible def and max tag components, def (%+v) is "+
+					"higher than max (%+v)",
+				def,
+				max,
+			)
+		}
+		if hasMin && def < min {
+			return fmt.Errorf(
+				"incompatible def and min tag components, def (%+v) is "+
+					"lower than min (%+v)",
+				def,
+				min,
+			)
+		}
+	}
+
 	for _, field := range fields {
 		isPtr := field.Kind() == reflect.Ptr
-
-		var err error
-
-		// Minimum value
-		_, hasMin := tags["min"]
-		min := float64(0)
-		if hasMin {
-			min, err = parseFloat64(tags["min"])
-			if err != nil {
-				return err
-			}
-		}
-
-		// Maximum value
-		_, hasMax := tags["max"]
-		max := float64(0)
-		if hasMax {
-			max, err = parseFloat64(tags["max"])
-			if err != nil {
-				return err
-			}
-		}
-
-		// Checking if minimum is not higher than maximum
-		if hasMax && hasMin && max < min {
-			return fmt.Errorf(
-				"max less than min on float64 field '%s' during struct sanitization",
-				field.Type().Name(),
-			)
-		}
-		// Checking if minimum and maximum are above 0
-		if (hasMin && min < 0) || (hasMax && max < 0) {
-			return fmt.Errorf(
-				"min and max on float64 field '%s' can not be below 0",
-				field.Type().Name(),
-			)
-		}
-
-		// Default value
-		_, hasDef := tags["def"]
-		def := float64(0)
-		if hasDef {
-			def, err = parseFloat64(tags["def"])
-			if err != nil {
-				return err
-			}
-
-			// Making sure default is not smaller than min or higher than max
-			if hasMax && def > max {
-				return fmt.Errorf(
-					"incompatible def and max tag components, def (%+v) is "+
-						"higher than max (%+v)",
-					def,
-					max,
-				)
-			}
-			if hasMin && def < min {
-				return fmt.Errorf(
-					"incompatible def and min tag components, def (%+v) is "+
-						"lower than min (%+v)",
-					def,
-					min,
-				)
-			}
-		}
 
 		// Pointer, nil, and we have a default: set it
 		if isPtr && field.IsNil() && hasDef {
