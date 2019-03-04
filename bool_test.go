1d31ff61f1f5946951f0487ef678e84d2e9c94f0
hscasn <henrique.coelho@groupbyinc.com>
2019-03-04 13:45:50 -0500

    bools

--- bool_test.go
+++ bool_test.go
@@ -130,3 +130,243 @@ func Test_sanitizeBoolField(t *testing.T) {
 		})
 	}
 }
+
+func Test_sanitizeBoolField_Slice(t *testing.T) {
+	s, _ := New()
+
+	type TestStrStructBoolSli struct {
+		Field []bool `san:""`
+	}
+	type TestStrStructBoolPtrSli struct {
+		Field []*bool `san:""`
+	}
+	type TestStrStructBoolPtrSliDef struct {
+		Field []*bool `san:"def=true"`
+	}
+	type TestStrStructBoolSliPtr struct {
+		Field *[]bool `san:""`
+	}
+	type TestStrStructBoolPtrSliPtr struct {
+		Field *[]*bool `san:""`
+	}
+	type TestStrStructBoolPtrSliPtrDef struct {
+		Field *[]*bool `san:"def=true"`
+	}
+
+	// Each *string test has isolated arguments and results, since the
+	// arguments will be mutated, they should not be reused
+	argString0 := false
+	resString0 := false
+	argString1 := true
+	resString1 := true
+	argString2 := false
+	resString2 := false
+	resString3 := true
+	argString4 := false
+	resString4 := false
+	argString5 := true
+	resString5 := true
+	argString6 := false
+	resString6 := false
+	resString7 := true
+
+	type args struct {
+		v   interface{}
+		idx int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    interface{}
+		wantErr bool
+	}{
+		{
+			name: "Applies tags to a non-empty []bool field.",
+			args: args{
+				v: &TestStrStructBoolSli{
+					Field: []bool{
+						false,
+						true,
+						false,
+					},
+				},
+				idx: 0,
+			},
+			want: &TestStrStructBoolSli{
+				Field: []bool{
+					false,
+					true,
+					false,
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "Applies tags to an empty []bool field.",
+			args: args{
+				v:   &TestStrStructBoolSli{},
+				idx: 0,
+			},
+			want:    &TestStrStructBoolSli{},
+			wantErr: false,
+		},
+		{
+			name: "Applies tags to a non-empty []*bool field.",
+			args: args{
+				v: &TestStrStructBoolPtrSli{
+					Field: []*bool{
+						&argString0,
+						&argString1,
+						&argString2,
+					},
+				},
+				idx: 0,
+			},
+			want: &TestStrStructBoolPtrSli{
+				Field: []*bool{
+					&resString0,
+					&resString1,
+					&resString2,
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "Applies tags to a non-empty []*bool field.",
+			args: args{
+				v: &TestStrStructBoolPtrSli{
+					Field: []*bool{
+						nil,
+						nil,
+						nil,
+					},
+				},
+				idx: 0,
+			},
+			want: &TestStrStructBoolPtrSli{
+				Field: []*bool{
+					nil,
+					nil,
+					nil,
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "Applies tags (with default) to a non-empty []*bool field.",
+			args: args{
+				v: &TestStrStructBoolPtrSliDef{
+					Field: []*bool{
+						nil,
+					},
+				},
+				idx: 0,
+			},
+			want: &TestStrStructBoolPtrSliDef{
+				Field: []*bool{
+					&resString3,
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "Applies tags to a non-empty *[]bool field.",
+			args: args{
+				v: &TestStrStructBoolSliPtr{
+					Field: &[]bool{
+						false,
+						true,
+						false,
+					},
+				},
+				idx: 0,
+			},
+			want: &TestStrStructBoolSliPtr{
+				Field: &[]bool{
+					false,
+					true,
+					false,
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "Applies tags to an empty *[]bool field.",
+			args: args{
+				v:   &TestStrStructBoolSliPtr{},
+				idx: 0,
+			},
+			want:    &TestStrStructBoolSliPtr{},
+			wantErr: false,
+		},
+		{
+			name: "Applies tags to a non-empty *[]*bool field.",
+			args: args{
+				v: &TestStrStructBoolPtrSliPtr{
+					Field: &[]*bool{
+						&argString4,
+						&argString5,
+						&argString6,
+					},
+				},
+				idx: 0,
+			},
+			want: &TestStrStructBoolPtrSliPtr{
+				Field: &[]*bool{
+					&resString4,
+					&resString5,
+					&resString6,
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "Applies tags to a non-empty *[]*bool field.",
+			args: args{
+				v: &TestStrStructBoolPtrSliPtr{
+					Field: &[]*bool{
+						nil,
+						nil,
+						nil,
+					},
+				},
+				idx: 0,
+			},
+			want: &TestStrStructBoolPtrSliPtr{
+				Field: &[]*bool{
+					nil,
+					nil,
+					nil,
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "Applies tags (with default) to a non-empty *[]*bool field.",
+			args: args{
+				v: &TestStrStructBoolPtrSliPtrDef{
+					Field: &[]*bool{
+						nil,
+					},
+				},
+				idx: 0,
+			},
+			want: &TestStrStructBoolPtrSliPtrDef{
+				Field: &[]*bool{
+					&resString7,
+				},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := sanitizeBoolField(*s, reflect.ValueOf(tt.args.v).Elem(), tt.args.idx); (err != nil) != tt.wantErr {
+				t.Errorf("sanitizeBoolField() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if !reflect.DeepEqual(tt.args.v, tt.want) {
+				t.Errorf("sanitizeBoolField() - failed field - got %+v but wanted %+v", tt.args.v, tt.want)
+			}
+		})
+	}
+}
